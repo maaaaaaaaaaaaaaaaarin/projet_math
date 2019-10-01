90e4d2ac65645a242aa088b82ab0025ae308f698
Marin <marin.ffxiv@gmail.com>
2019-10-01 08:34:46 +0200

    Similarité par utilisateur & préparation similarité par items

--- test_similarité.py
+++ test_similarité.py
@@ -12,7 +12,7 @@ personnes = loadAsList(fichier)
 
 def bravais_pearson(a, b, log=False, limit=-1):
     # Les sets de donnés doivent être de la même longueur
-    if len(a)!=len(b): return
+    # if len(a)!=len(b): return
 
     moyennes = []
 
@@ -73,16 +73,63 @@ def like(a,b):
             print("{}&{} --> très similaires ({:3.3})".format(i-1,i,r))
         #print()
 
-print(bravais_pearson(personnes[0],personnes[1]))
+#print(bravais_pearson(personnes[0],personnes[1]))
 
-    #print("Moyenne A: " + str(moy_A))
-    #print("Moyenne B: " + str(moy_B))
-    #print("diff: " + str(max([moy_A, moy_B])-min([moy_A, moy_B])))
-    #print("Len a: "+str(len(a)))
-    #print("Len b: "+str(len(b)))
+def colonne(n):
+    return list(map(lambda x: x[n], list(filter(lambda x: x[n] != -1, personnes))))
 
+"""
+    nUtilisateur    : int
+    item            : int
+
+    return          : int
+"""
+def prédire(nUtilisateur, item, approche="item"):
+    if approche == "utilisateur":
+        p = personnes[nUtilisateur]
+        m = sum(p)/len(p)
+
+        #print(str(nUtilisateur)+": "+str(p[item]))
+        # on filtre les personnes qui n'ont pas notées l'objet n°item
+        filtré = list(filter(lambda x: x[item] != -1, personnes))
+        
+        print("\nIl y a " + str(len(filtré))+" utilisateurs qui ont noté l'objet "+str(item))
+        print("\nL'utilisateur "+str(nUtilisateur)+" ", end="")
+        if p[item] == -1:
+            print("n'a pas noté l'objet "+str(item)+". ("+str(p[item])+")")
+        else:
+            print("a noté l'objet "+str(item)+". ("+str(p[item])+")")
+        print()
+
+
+        u = 0
+        d = 0
+        for i in list(filtré):
+            # r * objet donné moins moyenne utilisateur
+            r = bravais_pearson(p,i)
+            if r >= 0.8:
+                print(str(r) + "\t"+str(i[item]))
+                u +=  r * (i[item]-(sum(i)/len(i)))
+                d += abs(r)
+        return m + (u/d)
+    
+    if approche == "item":
+        p = personnes[nUtilisateur]
+        i = colonne(0)
+        m = sum(i) / len(i)
+
+
+
+
+
+print(colonne(0))
+print(colonne(1)) #FIXME
+
+
+#d = prédire(0, 3)
+#print("La note est " + str(round(d)) + " ( "+str(d)+" )")
 #Objectif
 # print(sum(list(map(lambda x: len(x), personnes))))
-#for i in range(1,10):
-#    print(bravais_pearson(personnes[i-1],personnes[i]))
+for i in range(1,10):
+    print(bravais_pearson(colonne(i-1), colonne(i)))
 #print(bravais_pearson(personnes[0],personnes[1]))
